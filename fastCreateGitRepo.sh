258943b772fd154a78c4e31b044e08446894edd9
adi.wu <adi.wu@kiwigame.com>
2024-08-17 18:46:23 +0800

    Initial commit

---
+++ fastCreateGitRepo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# auth adi
+
+# how to use: 
+# Update to your own GitHub username and repository name
+
+# basic source file
+# you can add your own alias... or just mark lines 9-10 
+source ~/.alias
+for file in /Users/adi/tools/sh/lib/*.zsh; do source $file; done
+
+# my-repo-name
+myreponame="$1"
+gitusername="kokp520"
+
+# true 為不存在, -n 為true代表存在
+if [ -z "$myreponame" ]; then 
+  echo "Please provide a repository name"
+  exit 1
+fi
+
+# create new repository on GitHub
+green "Creating new repository on GitHub..."
+gh repo create $1 --public --description "$2"
+ 
+# push local changes to remote repository
+green "Pushing local changes to remote repository..."
+git init 
+git add . 
+git commit -m "Initial commit" 
+git branch -M main 
+git remote add origin https://github.com/$gitusername/$1.git 
+git push -u origin main 
+
+green "Local changes pushed to remote repository Finished!"
+green "New repository created on GitHub: https://github.com/$gitusername/$1"
+
+green "Creating MIT..."
+license MIT
+
+# my alias print green text...
+green "Done"
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# install fastCreateGitRepo
+
+把腳本的gitusername改成自己的github帳號，然後執行以下指令：
+
+```
+brew install gh
+```
+
+```
+./fastCreateGitRepo <repo-name>
+```
+
+and Done!
